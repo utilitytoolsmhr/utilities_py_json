c387d9454e5829b5f49fb5733c5f9b9e3dbde093
utilitytoolsmhr <mairoborss@gmail.com>
2024-05-31 16:31:38 -0400

    Update standar.py

--- standar.py
+++ standar.py
@@ -47,7 +47,21 @@ def compare_json_structures(json_files):
 def make_serializable(obj):
     if isinstance(obj, (list, dict, str, int, float, bool, type(None))):
         return obj
+    if isinstance(obj, set):
+        return list(obj)
+    if isinstance(obj, tuple):
+        return list(obj)
+    if isinstance(obj, bytes):
+        return obj.decode('utf-8')
     return str(obj)
+
+def recursive_make_serializable(data):
+    if isinstance(data, dict):
+        return {key: recursive_make_serializable(value) for key, value in data.items()}
+    elif isinstance(data, list):
+        return [recursive_make_serializable(element) for element in data]
+    else:
+        return make_serializable(data)
 
 def main():
     persona_dir = "persona"
@@ -60,16 +74,16 @@ def main():
     complete_empresa_json = merge_json_data(empresa_json_files)
     
     with open('complete_persona.json', 'w') as f:
-        json.dump(complete_persona_json, f, indent=4, default=make_serializable)
+        json.dump(recursive_make_serializable(complete_persona_json), f, indent=4)
         
     with open('complete_empresa.json', 'w') as f:
-        json.dump(complete_empresa_json, f, indent=4, default=make_serializable)
+        json.dump(recursive_make_serializable(complete_empresa_json), f, indent=4)
     
     persona_differences = compare_json_structures(persona_json_files)
     empresa_differences = compare_json_structures(empresa_json_files)
     
-    persona_differences_serializable = [(i, make_serializable(diff)) for i, diff in persona_differences]
-    empresa_differences_serializable = [(i, make_serializable(diff)) for i, diff in empresa_differences]
+    persona_differences_serializable = [(i, recursive_make_serializable(diff)) for i, diff in persona_differences]
+    empresa_differences_serializable = [(i, recursive_make_serializable(diff)) for i, diff in empresa_differences]
     
     with open('persona_structure_differences.json', 'w') as f:
         json.dump(persona_differences_serializable, f, indent=4)
