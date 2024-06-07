2d9f2c92de98bb508115577ffa94c52cee77fda0
utilitytoolsmhr <mairoborss@gmail.com>
2024-06-07 16:41:31 -0400

    Update map_template.py

--- map_template.py
+++ map_template.py
@@ -3,9 +3,12 @@ import sys
 import traceback
 import json
 from jinja2 import Template
-
-# Importar funciones desde pe_utils.py
 from pe_utils import text_fix, float_fix, int_fix
+import re
+
+# Crear la carpeta /scripts si no existe
+output_dir = 'scripts'
+os.makedirs(output_dir, exist_ok=True)
 
 # Plantilla para los scripts Python
 template_script = """
@@ -39,28 +42,18 @@ def main(payload):
 
         # Data del módulo
         nodo = modulo[0].get('Data').get(target)
+        
+        # Aplicar funciones de pe_utils.py
+        for key, value in nodo.items():
+            if isinstance(value, str):
+                nodo[key] = text_fix(value)
+            elif isinstance(value, float):
+                nodo[key] = float_fix(value)
+            elif isinstance(value, int):
+                nodo[key] = int_fix(value)
+
     except:
         traceback.print_exc()
-
-    #################################################
-    ################### Functions ###################
-    #################################################
-
-    def process_data(data):
-        if isinstance(data, dict):
-            for key, value in data.items():
-                if isinstance(value, str):
-                    data[key] = text_fix(value)
-                elif isinstance(value, float):
-                    data[key] = float_fix(value)
-                elif isinstance(value, int):
-                    data[key] = int_fix(value)
-                elif isinstance(value, list) or isinstance(value, dict):
-                    process_data(value)
-        elif isinstance(data, list):
-            for item in data:
-                process_data(item)
-        return data
 
     #################################################
     ################# Data Processing ###############
@@ -72,7 +65,7 @@ def main(payload):
                     "Codigo": codigo,
                     "Nombre": modulo[0].get('Nombre'),
                     "Data": modulo[0].get('Data').get('flag'),
-                    "{{ target_key }}": process_data(nodo) if nodo else None
+                    "{{ target_key }}": nodo if nodo else None
                 }
             }
     except:
@@ -88,23 +81,25 @@ def main(payload):
 #################################################
 ################## TEST #########################
 #################################################
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Uso: python {{ script_name }}.py <archivo_json>")
-    else:
-        archivo_json = sys.argv[1]
-        with open(archivo_json, 'r', encoding="UTF-8") as file:
-            request = json.load(file)
-            out = json.dumps(main(request), indent=4)
-            print(out)
-            with open('respond.json', 'w', encoding="UTF-8") as outfile:
-                outfile.write(out)
+if len(sys.argv) != 2:
+    print("Uso: python {{ script_name }}.py <archivo_json>")
+else:
+    archivo_json = sys.argv[1]
+    with open(archivo_json, 'r', encoding="UTF-8") as file:
+        request = json.load(file)
+        out = json.dumps(main(request), indent=4)
+        print(out)
+        with open('respond.json', 'w', encoding="UTF-8") as outfile:
+            outfile.write(out)
 """
 
 def read_json(file_path):
     with open(file_path, 'r', encoding="UTF-8") as file:
         data = json.load(file)
     return data
+
+def to_snake_case(name):
+    return re.sub(r'\s+', '_', re.sub(r'([a-z])([A-Z])', r'\1_\2', name)).lower()
 
 def generate_script(module_name, target_name, target_key, script_name, codigo_persona, codigo_empresa):
     template = Template(template_script)
@@ -117,7 +112,7 @@ def generate_script(module_name, target_name, target_key, script_name, codigo_pe
         codigo_empresa=codigo_empresa
     )
     
-    script_path = f"pe_modulo_{script_name}.py"
+    script_path = os.path.join(output_dir, f"pe_modulo_{script_name}.py")
     with open(script_path, 'w', encoding="UTF-8") as file:
         file.write(script_content)
     
@@ -125,29 +120,40 @@ def generate_script(module_name, target_name, target_key, script_name, codigo_pe
 
 def get_modules_info(json_data, module_name):
     modulos = json_data.get('dataSourceResponse').get('GetReporteOnlineResponse').get('ReporteCrediticio').get('Modulos').get('Modulo')
+    print(f"Buscando módulo: {module_name}")
     for modulo in modulos:
+        print(f"Módulo encontrado: {modulo.get('Nombre')}")
         if modulo.get('Nombre') == module_name:
             codigo_persona = modulo.get('Codigo')
-            target_name = list(modulo.get('Data').keys())[1]  # Asumimos que el primer key es 'flag'
+            target_name = next((key for key in modulo.get('Data').keys() if key != 'flag'), None)  # Ignorar 'flag'
             return codigo_persona, target_name
     return None, None
 
-# Archivos JSON de entrada
+# Archivos JSON de entrada desde la ruta de ejecución
 persona_json = read_json('persona.json')
 empresa_json = read_json('empresa.json')
 
-# Módulos a generar
+# Módulos a generar con nombres exactos ajustados
 modules = [
     {"module_name": "Resumen Flag", "script_name": "resumen_flags"},
-    {"module_name": "Score Predictivo con Variables", "script_name": "resumen_score"},
-    {"module_name": "Representantes Legales", "script_name": "representantes_legales"},
-    {"module_name": "Registro Crediticio Consolidado (RCC)", "script_name": "registro_crediticio_consolidado"},
-    {"module_name": "Sistema Financiero Regulado (SBS) y No Regulado (Microfinanzas)", "script_name": "sistema_financiero"}
+    {"module_name": "SCORE PREDICTIVO CON VARIABLES", "script_name": "score_predictivo_con_variables"},
+    {"module_name": "REPRESENTANTES LEGALES", "script_name": "representantes_legales"},
+    {"module_name": "Registro Crediticio Consolidado (Rcc)", "script_name": "registro_crediticio_consolidado_rcc"},
+    {"module_name": "SISTEMA FINANCIERO REGULADO (SBS) Y NO REGULADO (MICROFINANZAS)", "script_name": "sistema_financiero_regulado_y_no_regulado"}
 ]
 
 # Generar los scripts para cada módulo
 for module in modules:
     codigo_persona, target_name = get_modules_info(persona_json, module["module_name"])
+    if not target_name:
+        print(f"Error: No se encontró el target para el módulo {module['module_name']} en persona.json")
+        continue
+    
     codigo_empresa, _ = get_modules_info(empresa_json, module["module_name"])
-    target_key = target_name.lower()  # Asumimos que el key es el nombre del target en minúsculas
-    generate_script(module["module_name"], target_name, target_key, module["script_name"], codigo_persona, codigo_empresa)
+    if not target_name:
+        print(f"Error: No se encontró el target para el módulo {module['module_name']} en empresa.json")
+        continue
+
+    target_key = to_snake_case(target_name)  # Convertir el nombre del target a snake_case
+    script_name = to_snake_case(module["module_name"])  # Convertir el nombre del módulo a snake_case
+    generate_script(module["module_name"], target_name, target_key, script_name, codigo_persona, codigo_empresa)
