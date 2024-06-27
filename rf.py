476283f0c3ac7982a498d525302ee965ec783809
utilitytoolsmhr <mairoborss@gmail.com>
2024-06-27 16:52:37 -0400

    Update rf.py

--- rf.py
+++ rf.py
@@ -29,8 +29,8 @@ def main(payload):
         xsi_to_null(payload)
 
         # Seleccionamos el modulo target
-        nombre = 'OTRAS DEUDAS IMPAGAS'
-        target = 'OtrasDeudasImpagas'
+        nombre = 'DEUDAS IMPAGAS'
+        target = 'DeudasImpagas'
         codigo = codigo_modulo
         modulos = payload.get('dataSourceResponse').get('GetReporteOnlineResponse').get('ReporteCrediticio').get('Modulos').get('Modulo')
         modulo = [modulo for modulo in modulos if modulo.get('Data') is not None and nombre in modulo.get('Nombre')]
@@ -49,6 +49,27 @@ def main(payload):
     #################################################
     ################### Variables ###################
     #################################################
+
+    def producto_deuda(nodo):
+        return {
+            'CodigoProducto': text_fix(nodo.get('CodigoProducto')),
+            'DeudaSoles': float_fix(nodo.get('DeudaSoles')),
+            'DeudaDolares': float_fix(nodo.get('DeudaDolares'))
+        }
+
+    def semaforo_periodo(nodo):
+        return {
+            'periodo': text_fix(nodo.get('periodo')),
+            'TieneDeuda': bool(nodo.get('TieneDeuda')),
+            'DetalleProductos': {
+                'ProductoDeuda': [producto_deuda(item) for item in nodo.get('DetalleProductos', {}).get('ProductoDeuda', [])]
+            }
+        }
+
+    def resumen_deudas_impagas(nodo):
+        return {
+            'SemaforoPeriodo': [semaforo_periodo(item) for item in nodo.get('SemaforoPeriodo', [])]
+        }
 
     def deuda_detalle(nodo):
         return {
@@ -182,18 +203,81 @@ def main(payload):
             'MontoSoles': float_fix(nodo.get('MontoSoles')),
             'CantidadDolares': int_fix(nodo.get('CantidadDolares')),
             'MontoDolares': float_fix(nodo.get('MontoDolares')),
-            'Detalle': nodo.get('Detalle', {})
+            'Detalle': {
+                'Deuda': [
+                    {
+                        'FechaCreacion': text_fix(nodo.get('FechaCreacion')),
+                        'NumeroExpediente': text_fix(nodo.get('NumeroExpediente')),
+                        'PensionMensual': float_fix(nodo.get('PensionMensual')),
+                        'MontoImporteAdeudado': float_fix(nodo.get('MontoImporteAdeudado')),
+                        'Divisa': text_fix(nodo.get('Divisa'))
+                    }
+                ]
+            }
+        }
+
+    def vista_historica_detalle(nodo):
+        return {
+            'periodo': text_fix(nodo.get('periodo')),
+            'TieneDeuda': bool(nodo.get('TieneDeuda')),
+            'InfocorpDeudaSoles': float_fix(nodo.get('InfocorpDeudaSoles')),
+            'InfocorpDeudaDolares': float_fix(nodo.get('InfocorpDeudaDolares')),
+            'NegSUNATDeudaSoles': float_fix(nodo.get('NegSUNATDeudaSoles'))
         }
 
     def inquilinos_morosos_detalle(nodo):
         return {
-            'Cabecera': nodo.get('Cabecera', {}),
-            'Detalle': nodo.get('Detalle', {})
+            'FechaDeudaVencidaReciente': text_fix(nodo.get('FechaDeudaVencidaReciente')),
+            'CantidadSoles': int_fix(nodo.get('CantidadSoles')),
+            'MontoSoles': float_fix(nodo.get('MontoSoles')),
+            'CantidadDolares': int_fix(nodo.get('CantidadDolares')),
+            'MontoDolares': float_fix(nodo.get('MontoDolares')),
+            'Detalle': {
+                'Deuda': [
+                    {
+                        'FechaVencimiento': text_fix(nodo.get('FechaVencimiento')),
+                        'FechaRegistro': text_fix(nodo.get('FechaRegistro')),
+                        'ArrendadorCodigoSci': text_fix(nodo.get('ArrendadorCodigoSci')),
+                        'ArrendadorTipoDocumento': text_fix(nodo.get('ArrendadorTipoDocumento')),
+                        'ArrendadorNroDocumento': text_fix(nodo.get('ArrendadorNroDocumento')),
+                        'ArrendadorNombreRazonSocial': text_fix(nodo.get('ArrendadorNombreRazonSocial')),
+                        'ArrendadorDireccion': text_fix(nodo.get('ArrendadorDireccion')),
+                        'ArrendadorDistrito': text_fix(nodo.get('ArrendadorDistrito')),
+                        'ArrendadorProvincia': text_fix(nodo.get('ArrendadorProvincia')),
+                        'ArrendadorDepartamento': text_fix(nodo.get('ArrendadorDepartamento')),
+                        'ArrendadorTelefono': text_fix(nodo.get('ArrendadorTelefono')),
+                        'ArrendadorCelular': text_fix(nodo.get('ArrendadorCelular')),
+                        'ArrendadorMail': text_fix(nodo.get('ArrendadorMail')),
+                        'ArrendadorNroDocumentoMoroso': text_fix(nodo.get('ArrendadorNroDocumentoMoroso')),
+                        'ArrendatarioTipoDocumento': text_fix(nodo.get('ArrendatarioTipoDocumento')),
+                        'ArrendatarioNroDocumento': text_fix(nodo.get('ArrendatarioNroDocumento')),
+                        'ArrendatarioApePaterno': text_fix(nodo.get('ArrendatarioApePaterno')),
+                        'ArrendatarioApeMaterno': text_fix(nodo.get('ArrendatarioApeMaterno')),
+                        'ArrendatarioNombre': text_fix(nodo.get('ArrendatarioNombre')),
+                        'ArrendatarioRazonSocial': text_fix(nodo.get('ArrendatarioRazonSocial')),
+                        'ArrendatarioDireccion': text_fix(nodo.get('ArrendatarioDireccion')),
+                        'ArrendatarioDistrito': text_fix(nodo.get('ArrendatarioDistrito')),
+                        'ArrendatarioProvincia': text_fix(nodo.get('ArrendatarioProvincia')),
+                        'ArrendatarioDepartamento': text_fix(nodo.get('ArrendatarioDepartamento')),
+                        'ArrendatarioDireccionArrendada': text_fix(nodo.get('ArrendatarioDireccionArrendada')),
+                        'ArrendatarioDistritoArrendada': text_fix(nodo.get('ArrendatarioDistritoArrendada')),
+                        'ArrendatarioProvinciaArrendada': text_fix(nodo.get('ArrendatarioProvinciaArrendada')),
+                        'ArrendatarioDepartamentoArrendada': text_fix(nodo.get('ArrendatarioDepartamentoArrendada')),
+                        'ArrendatarioFechaVencimiento': text_fix(nodo.get('ArrendatarioFechaVencimiento')),
+                        'ArrendatarioTipoMoneda': text_fix(nodo.get('ArrendatarioTipoMoneda')),
+                        'ArrendatarioMontoImpago': float_fix(nodo.get('ArrendatarioMontoImpago')),
+                        'ArrendatarioTipoRegistro': text_fix(nodo.get('ArrendatarioTipoRegistro')),
+                        'ArrendatarioCelular': text_fix(nodo.get('ArrendatarioCelular')),
+                        'ArrendatarioMail': text_fix(nodo.get('ArrendatarioMail'))
+                    }
+                ]
+            }
         }
 
-    def otras_deudas_impagas(nodo):
+    def deudas_impagas(nodo):
         return {
-            'Sicom': [sicom_detalle(item) for item in nodo.get('Sicom', [])],
+            'ResumenDeudasImpagas': resumen_deudas_impagas(nodo.get('ResumenDeudasImpagas', {})),
+            'Sicom': sicom_detalle(nodo.get('Sicom', {})),
             'NegativoSunat': negativo_sunat_detalle(nodo.get('NegativoSunat', {})),
             'Omisos': {
                 'Cabecera': {
@@ -210,10 +294,13 @@ def main(payload):
             'CuentasCerradas': cuentas_cerradas_detalle(nodo.get('CuentasCerradas', {})),
             'TarjetasAnuladas': tarjetas_anuladas_detalle(nodo.get('TarjetasAnuladas', {})),
             'Redam': redam_detalle(nodo.get('Redam', {})),
+            'VistaHistorica': {
+                'Periodo': [vista_historica_detalle(item) for item in nodo.get('VistaHistorica', {}).get('Periodo', [])]
+            },
             'InquilinosMorosos': inquilinos_morosos_detalle(nodo.get('InquilinosMorosos', {}))
         }
 
-    data_output = otras_deudas_impagas(nodo)
+    data_output = deudas_impagas(nodo)
 
     # Limpiar objetos vacíos
     def clean_data(data):
@@ -237,7 +324,7 @@ def main(payload):
                 "Nombre": modulo[0].get('Nombre'),
                 "Data": {
                     "flag": modulo[0].get('Data').get('flag'),
-                    "OtrasDeudasImpagas": data_output
+                    "DeudasImpagas": data_output
                 }
             }
         except Exception as e:
@@ -248,18 +335,18 @@ def main(payload):
                 "Nombre": nombre,
                 "Data": {
                     "flag": False,
-                    "OtrasDeudasImpagas": {}
+                    "DeudasImpagas": {}
                 }
             }
     else:
         try:
             final_out = {
-                "OtrasDeudasImpagas": {
+                "DeudasImpagas": {
                     "Codigo": modulo[0].get('Codigo'),
                     "Nombre": modulo[0].get('Nombre'),
                     "Data": {
                         "flag": modulo[0].get('Data').get('flag'),
-                        "OtrasDeudasImpagas": data_output
+                        "DeudasImpagas": data_output
                     }
                 }
             }
@@ -267,12 +354,12 @@ def main(payload):
             print(f"Error generando la salida final (formato_salida=True): {e}")
             traceback.print_exc()
             final_out = {
-                "OtrasDeudasImpagas": {
+                "DeudasImpagas": {
                     "Codigo": codigo,
                     "Nombre": nombre,
                     "Data": {
                         "flag": False,
-                        "OtrasDeudasImpagas": {}
+                        "DeudasImpagas": {}
                     }
                 }
             }
